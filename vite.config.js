a0ee32ff510480a69637f115327116a68a8eea84
juangiraaaldo <juangiraaaldo10@gmail.com>
2026-04-15 19:57:14 -0500

    Rutas vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taller3-React/'
+  // Si estamos en GitHub Pages (entorno de producción de GH), usa el nombre del repo.
+  // Si no (como en Vercel o Local), usa la raíz '/'.
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL
+  ? '/Taller3-React/'
+  : '/',
 })
